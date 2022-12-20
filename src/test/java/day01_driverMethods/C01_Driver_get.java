e266ad6c7d908e72a3f9a1dccdc6b079aa548790
GalipBulan <galipbulan@gmail.com>
2022-12-20 18:41:54 +0100

    day1

--- src/test/java/day01_driverMethods/C01_Driver_get.java
+++ src/test/java/day01_driverMethods/C01_Driver_get.java
@@ -7,21 +7,40 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class C01_Driver_get {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
 
-        WebDriver driver1 = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
 
-        driver1.get("https://www.amazon.com");
+        driver.get("https://www.amazon.com");
+
+        Thread.sleep(5000);
+
+        driver.close();
+
+        /*
+            1- Selenium jar dosyalarini ve chromedriver driver'ini manuel olarak sisteme
+            tanitmis olduk.
+            Eger firefox veya safari ile calismamiz gerekse,
+            bu defa da onlarin driver'larini indirip, manuel olarak
+            10.satirda o driver'lari tanimlamaliyiz
+            2- 13.satirda ChromeDriver() constructor'ini kullanarak bir obje olusturduk
+            bu objeyi kullanmasak bile
+            bu satirdan dolayi bos bir browser acilir
+            3- driver.get(url) driver'i yazdigimiz url'e goturur
+            url yazarken www yazmasak calisir
+            ancak https:// yazmazsak method calismaz
+         */
+
 
 
 
         System.setProperty("webdriver.firefox.driver","C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs");
 
-        WebDriver driver2 = new FirefoxDriver();
+        WebDriver driver1 = new FirefoxDriver();
 
-        driver2.get("https://www.amazon.com");
+        driver1.get("https://www.amazon.com");
 
 
 
